c03e11d181bee92832a7a2cb3ae13269ef250898
ADARSH S R <57584265+adhi404121@users.noreply.github.com>
2022-10-09 07:32:37 -0700

    Add files via upload

---
+++ patch newgame.py
@@ -0,0 +1,10 @@
+
+a="thaankal a choose cheythennu kettu kammi akki choyik"
+b="thaankal b choose cheythennu kettu kammi akki choyik"
+user=(input("what you like a or b="))
+if(user=="a"):
+    print(a)
+if(user=="b"):
+    print(b)
+else :
+    print ("poda manda poi a or b select chyyeda")
