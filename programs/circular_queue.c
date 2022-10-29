85242e2d0814d3bd47be6c51e6cd011ad7486c69
Albin Varghese <albin8285@gmail.com>
2022-10-29 12:04:07 +0530

    fix: add display method and fix some known issues

--- programs/circular_queue.c
+++ programs/circular_queue.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 //variables.
-int maxsize, front = -1, rear = -1, Q[50], choice, exited = 0, item;
+int maxsize, front = -1, rear = -1, Q[50], choice, exited = 0, item, i;
 
 //operations.
 void insert();
 void delete();
 void display();
+void exitProgram();
 
 void makeSelection();
 
@@ -47,6 +48,9 @@ void makeSelection()
         case 3 :
             display();
         break;
+        case 4 :
+            exitProgram();
+        break;
         default :
             printf("Invalid selection, Please try again...\n");
     }
@@ -77,20 +81,49 @@ void delete()
 {
     if(front == -1)
     {
-        printf("\n\n Queue is empty \n\n");
+        printf("\n\n Queue is empty..\n");
     } else {
         if(front == rear)
         {
+            front = -1;
+            rear = -1;
             printf("\n\n Queue resetted.\n\n");
         }
 
         item = Q[front];
-        printf("%d is deleted from Queue");
-        front = (front + 1) % 4;
+        printf("%d is deleted from Queue", item);
+        front = (front + 1) % maxsize;
     }
 }
 
 void display()
 {
-    
+    if(front == -1)
+        printf("\nQueue is empty...\n");
+    else
+    {
+        printf("\n\n Elements in Queue : ");
+        if(front <= rear)
+        {
+            for(i = front; i <= rear; i++)
+            {
+                printf("%d ", Q[i]);
+            }
+        } else if(front > rear){
+            for(i = front; i <= maxsize-1; i++)
+            {
+                printf("%d ", Q[i]);
+            }
+            for(i = 0; i <= rear; i++)
+            {
+                printf("%d ", Q[i]);
+            }
+        }
+    }
+}
+
+void exitProgram()
+{
+    exited = 1;
+    printf("\nBye... ;) \n");
 }
